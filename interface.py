3ca5d1a5b6a465f56c865ed5a16d6113ab7ac1ac
lombardi-g <gabriellomba@gmail.com>
2023-11-08 17:02:15 -0300

    Added error handling block #6

--- interface.py
+++ interface.py
@@ -1,4 +1,5 @@
 from scraping import scrape_match_summary, excel_file
+import time, sys
 
 def assertURL():
     print("Colar link da súmula, confirmar com Enter: ")
@@ -7,11 +8,23 @@ def assertURL():
         if pasted_URL.startswith("https://egol.fcf.com.br/SISGOL"):
             return pasted_URL
         else:
-            print("Inválido")
+            print("Inválido, colar link da súmula da FCF")
 
 
 if __name__ == "__main__":
-    print(f'Leitor de súmulas da FCF.\nPara funcionamento correto:\n1 - A base de dados deve estar com o nome: {excel_file}\n2 - O arquivo {excel_file} e este programa devem estar na mesma pasta\n')
-    validURL = assertURL()
-    scrape_match_summary(validURL)
-    print("\nConcluído.")
+    try:
+        print(f'Leitor de súmulas da FCF.\nPara funcionamento correto:\n- A base de dados deve estar com o nome: {excel_file}\n- O arquivo {excel_file} e este programa devem estar na mesma pasta\n')
+        validURL = assertURL()
+        scrape_match_summary(validURL)
+        print("\nConcluído.")
+    except FileNotFoundError:
+        print(f'\nO arquivo não está na pasta ou nome {excel_file} está incorreto.')
+        time.sleep(3)
+    except KeyboardInterrupt:
+        print("\nOperação cancelada.")
+    except AttributeError:
+        print("Erro na leitura da súmula.")
+        time.sleep(1)
+    
+    time.sleep(2)
+    sys.exit()
