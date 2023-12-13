7e0386d7ec7492378e9c796d8f95ff2c58578ec6
lombardi-g <gabriellomba@gmail.com>
2023-12-13 09:45:18 -0300

    #8 substitutions repetition structure

--- minutes.py
+++ minutes.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 
 #Same from scraping, use import later after testing
-url = 'https://egol.fcf.com.br/SISGOL/WDER0700_Sumula.asp?SelStart1=2023&SelStop1=2023&SelStart2=557&SelStop2=557&SelStart3=67&SelStop3=67&Index=1&RunReport=Run+Report'
+url = 'https://egol.fcf.com.br/SISGOL/WDER0700_Sumula.asp?SelStart1=2023&SelStop1=2023&SelStart2=505&SelStop2=505&SelStart3=85&SelStop3=85&Index=1&RunReport=Run+Report'
 response = requests.get(url)
 targetURL = BeautifulSoup(response.text, 'html.parser')
 
@@ -12,10 +12,14 @@ initial_lineup_locator = targetURL.find(name="td",string="3.0 - RELAÇÃO DE JOG
 initial_lineup_locator = initial_lineup_locator.find_next(name="td")
 
 # Read substitutions and make adding and subtractions
-substitutions_begin = targetURL.find(name="td", string="12.0 - SUBSTITUIÇÕES")
-substitutions_begin = substitutions_begin.find_next(name="td",string="Saiu").find_next(name="td")
-substitutions_end = substitutions_begin.find_next(name="td",string="**1T = 1° Tempo | 2T = 2° Tempo | INT = Intervalo")
+substitutions_locator = targetURL.find(name="td", string="12.0 - SUBSTITUIÇÕES")
+substitutions_locator = substitutions_locator.find_next(name="td",string="Saiu").find_next(name="td")
+substitutions_end = substitutions_locator.find_next(name="td",string="**1T = 1° Tempo | 2T = 2° Tempo | INT = Intervalo")
+while substitutions_locator is not substitutions_end:
+    substitutions_locator = substitutions_locator.find_next(name="td")
 
 # Store all in variables
 
 # Pass information to excel
+
+print()
