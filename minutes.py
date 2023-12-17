e04e6bf2e83a945e1a8007ff4b78b8c37bb4a1e1
lombardi-g <gabriellomba@gmail.com>
2023-12-17 20:06:53 -0300

    #8 storing info in list

--- minutes.py
+++ minutes.py
@@ -33,11 +33,10 @@ while not initial_lineup_locator.get_text().startswith("Capitão:"):
 initial_lineup = [name.rstrip() for name in initial_lineup]
 
 substitutions_locator = targetURL.find(name="td", string="12.0 - SUBSTITUIÇÕES")
-substitutions_locator = substitutions_locator.find_next(name="td",string="Saiu")
+substitutions_locator = substitutions_locator.find_next(name="td",string="Saiu").find_next(name="td")
 substitutions_end = substitutions_locator.find_next(name="td",string="**1T = 1° Tempo | 2T = 2° Tempo | INT = Intervalo")
-subs = {}
+subs = []
 while substitutions_locator is not substitutions_end:
-    substitutions_locator = substitutions_locator.find_next(name="td")
     minute_entered = substitutions_locator.get_text()
 
     substitutions_locator = substitutions_locator.find_next(name="td")
@@ -54,11 +53,18 @@ while substitutions_locator is not substitutions_end:
     leaving_player_jersey = substitutions_locator.get_text().split(" - ")[0]
     leaving_player_name = substitutions_locator.get_text().split(" - ")[1]
 
+    subs.append({"half":which_half,
+                 "minutes":minute_entered,
+                 "name":entering_player_name,
+                 "replacing":leaving_player_name}) if team == "FIGUEIRENSE" else None
 
+    substitutions_locator = substitutions_locator.find_next(name="td")
+
+# Final list, total match time of initial lineup subtracting subs minutes
 
 # Pass information to excel
 
-'''
- Debugging with prints
-print(initial_lineup)
-'''
+# Debugging with prints
+# print(initial_lineup)
+# print(len(subs))
+# print(subs)
