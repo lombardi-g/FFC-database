45a531d1775fa7eb30286669e5f1fc9123d515b6
lombardi-g <gabriellomba@gmail.com>
2024-01-05 10:32:27 -0300

    Adapting excel function

--- minutes.py
+++ minutes.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from openpyxl import load_workbook
 
 # Hard coding for tests. Use scraping.py variables
 url = 'https://egol.fcf.com.br/SISGOL/WDER0700_Sumula.asp?SelStart1=2023&SelStop1=2023&SelStart2=505&SelStop2=505&SelStart3=85&SelStop3=85&Index=1&RunReport=Run+Report'
@@ -8,7 +9,7 @@ targetURL = BeautifulSoup(response.text, 'html.parser')
 
 home = 'Fora'
 first_half_minutes = 47
-second_half_minutes = 61
+second_half_minutes = 51
 match = first_half_minutes+second_half_minutes
 
 initial_lineup_locator = targetURL.find(name="td",string="3.0 - RELAÇÃO DE JOGADORES")#.find_next(string="FIGUEIRENSE")
@@ -71,51 +72,17 @@ for players in subs:
         case "2":
             players["minutes"] = int(second_half_minutes) - players["minutes"]
 
-# Return list to use pass_to_excel function
+# Final list. Subtract minutes of starters that left.
 
+    excel_file = "MinutagemBase2023.xlsx"
     workbook = load_workbook(excel_file)
-    sheet = workbook['Minutagem']
+    sheet = workbook['Base23']
     # Hard-coded labels from sheet. Read them and dynamically make the dict?
     column_labels = {
-        "CÓDIGO JOGO":1,
-        "DATA JOGO":2,
-        "TREINADOR":3,
-        "CATEGORIA":4,
-        "COMPETIÇÃO":5,
-        "FIGUEIRENSE":6,
-        "G.F.":7,
-        "G.A.":8,
-        "ADVERSÁRIO":9,
-        "MANDO":10,
-        "LOCAL":11,
-        "Cidade":12,
-        "UF":13,
-        "JOGOS":14,
-        "VITÓRIA":15,
-        "EMPATE":16,
-        "DERROTA":17,
-        "MINUTOS JOGADOS":18,
-        "1º A MARCAR FIGUEIRENSE":19,
-        "1º A MARCAR ADVERSÁRIO":20,
-        "GOLS MARCADOS 1ºT - 0'-15'":21,
-        "GOLS MARCADOS 1ºT - 15'-30'":22,
-        "GOLS MARCADOS 1ºT - 30'-45'":23,
-        "GOLS MARCADOS 2ºT - 0'-15'":24,
-        "GOLS MARCADOS 2ºT - 15'-30'":25,
-        "GOLS MARCADOS 2ºT - 30'-45'":26,
-        "GOLS SOFRIDOS 1ºT - 0'-15'":27,
-        "GOLS SOFRIDOS 1ºT - 15'-30'":28,
-        "GOLS SOFRIDOS 1ºT - 30'-45'":29,
-        "GOLS SOFRIDOS 2ºT - 0'-15'":30,
-        "GOLS SOFRIDOS 2ºT - 15'-30'":31,
-        "GOLS SOFRIDOS 2ºT - 30'-45'":32
-    }
-    staff = {
-        "Sub14":"Guilherme Pereira",
-        "Sub15":"Guilherme Pereira",
-        "Sub17":"Lucas Ligio",
-        "Sub20":"Jhonatas Reis",
-        "Sub21":"Jhonatas Reis"
+        "Data":1,
+        "Adversário":2,
+        "Nome":3,
+        "Minutos":4,        
     }
 
     last_row = sheet.max_row
@@ -126,41 +93,13 @@ for players in subs:
             break
         last_row -= 1
         
-    last_row_value = sheet.cell(row=last_row, column=column_labels['CÓDIGO JOGO']).value
+    last_row_value = sheet.cell(row=last_row, column=column_labels['Data']).value
     
     new_row = last_row + 1
-    sheet.cell(row=new_row, column=column_labels['CÓDIGO JOGO'],value=last_row_value+1)
-    sheet.cell(row=new_row, column=column_labels['DATA JOGO'],value=date)
-    sheet.cell(row=new_row, column=column_labels['TREINADOR'],value=staff[category])
-    sheet.cell(row=new_row, column=column_labels['CATEGORIA'],value=category)
-    sheet.cell(row=new_row, column=column_labels['COMPETIÇÃO'],value=tournament)
-    sheet.cell(row=new_row, column=column_labels['FIGUEIRENSE'],value="Figueirense")
-    sheet.cell(row=new_row, column=column_labels['G.F.'],value=figueira_final_score)
-    sheet.cell(row=new_row, column=column_labels['G.A.'],value=opponent_final_score)
-    sheet.cell(row=new_row, column=column_labels['ADVERSÁRIO'],value=opponent)
-    sheet.cell(row=new_row, column=column_labels['MANDO'],value=home)
-    sheet.cell(row=new_row, column=column_labels['LOCAL'],value=place)
-    sheet.cell(row=new_row, column=column_labels['Cidade'],value=city)
-    sheet.cell(row=new_row, column=column_labels['UF'],value="SC")
-    sheet.cell(row=new_row, column=column_labels['JOGOS'],value=1)
-    sheet.cell(row=new_row, column=column_labels['VITÓRIA'],value=1 if figueira_final_score > opponent_final_score else 0)
-    sheet.cell(row=new_row, column=column_labels['EMPATE'],value=1 if figueira_final_score == opponent_final_score else 0)
-    sheet.cell(row=new_row, column=column_labels['DERROTA'],value=1 if figueira_final_score < opponent_final_score else 0)
-    sheet.cell(row=new_row, column=column_labels['MINUTOS JOGADOS'],value = int(first_half_minutes) + int(second_half_minutes))
-    sheet.cell(row=new_row, column=column_labels['1º A MARCAR FIGUEIRENSE'],value=1 if figueira_first == True else 0)
-    sheet.cell(row=new_row, column=column_labels['1º A MARCAR ADVERSÁRIO'],value=1 if figueira_first == False else 0)
-    sheet.cell(row=new_row, column=column_labels['GOLS MARCADOS 1ºT - 0\'-15\''], value= scored_list[0])
-    sheet.cell(row=new_row, column=column_labels['GOLS MARCADOS 1ºT - 15\'-30\''],value= scored_list[1])
-    sheet.cell(row=new_row, column=column_labels['GOLS MARCADOS 1ºT - 30\'-45\''],value= scored_list[2])
-    sheet.cell(row=new_row, column=column_labels['GOLS MARCADOS 2ºT - 0\'-15\''], value= scored_list[3])
-    sheet.cell(row=new_row, column=column_labels['GOLS MARCADOS 2ºT - 15\'-30\''],value= scored_list[4])
-    sheet.cell(row=new_row, column=column_labels['GOLS MARCADOS 2ºT - 30\'-45\''],value= scored_list[5])
-    sheet.cell(row=new_row, column=column_labels['GOLS SOFRIDOS 1ºT - 0\'-15\''], value= conceded_list[0])
-    sheet.cell(row=new_row, column=column_labels['GOLS SOFRIDOS 1ºT - 15\'-30\''],value= conceded_list[1])
-    sheet.cell(row=new_row, column=column_labels['GOLS SOFRIDOS 1ºT - 30\'-45\''],value= conceded_list[2])
-    sheet.cell(row=new_row, column=column_labels['GOLS SOFRIDOS 2ºT - 0\'-15\''], value= conceded_list[3])
-    sheet.cell(row=new_row, column=column_labels['GOLS SOFRIDOS 2ºT - 15\'-30\''],value= conceded_list[4])
-    sheet.cell(row=new_row, column=column_labels['GOLS SOFRIDOS 2ºT - 30\'-45\''],value= conceded_list[5])
+    sheet.cell(row=new_row, column=column_labels['Data'],value = ...)
+    sheet.cell(row=new_row, column=column_labels['Adversário'],value = ...)
+    sheet.cell(row=new_row, column=column_labels['Nome'],value = ...)
+    sheet.cell(row=new_row, column=column_labels['Minutos'],value = ...)
 
     workbook.save("Banco de Dados Figueirense Base.xlsx")
 
